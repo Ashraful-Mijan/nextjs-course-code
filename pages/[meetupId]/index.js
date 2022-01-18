9f22771f6dd50ef33ab7d857bf9979f4f9c51c9b
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2022-01-18 15:11:18 +0600

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+function MeetupDetails() {
+    return <MeetupDetail 
+    image=''
+    title='first meetup'
+    address='some street 5, some city'
+    description='thllo first meetup' 
+    />
+}
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import MeetupList from '../components/meetups/MeetupList';
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://cdn.londonandpartners.com/-/media/images/london/visit/things-to-do/sightseeing/25549367-emirates-airline-cable-car-640x360.jpg?w=310&hash=EAFEA9B59F590FF4EA2E4D88415E8DBACD09CB46',
+        address: 'Some address 5, 12345 Some',
+        description: 'this is a first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://cdn.londonandpartners.com/-/media/images/london/visit/things-to-do/sightseeing/25549367-emirates-airline-cable-car-640x360.jpg?w=310&hash=EAFEA9B59F590FF4EA2E4D88415E8DBACD09CB46',
+        address: 'Some address 6f, 123dd Some city',
+        description: 'this is a second meetup'
+    }
+]
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+export default HomePage;
